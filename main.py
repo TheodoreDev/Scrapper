72d6cc717828f91f65660b420b9aa41efd20de53
TheodoreDev <theodore.delbove@gmail.com>
2023-06-15 17:03:53 +0200

    scrapping

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import csv
 import time
 
 
-dataFile = "data/data.csv"
-header = ["Username", "Favorite"]
-Username = ["Test", "Test2", "Test3"]
-Favorite = ["True", "False", "True"]
-number_user = len(Username)
-
 while True:
+    dataFile = "data/data.csv"
+    header = ["Country", "Inhabitants"]
+    Country = ["Test", "Test2", "Test3"]
+    Inhabitants = ["1000", "120", "3500"]
+    number_user = len(Country)
+
     with open(dataFile, "w", newline="") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
         writer.writerow(header)
         for i in range(number_user):
-            writer.writerow([f"{Username[i]}", f"{Favorite[i]}"])
+            writer.writerow([f"{Country[i]}", f"{Inhabitants[i]}"])
 
     print("update")
     time.sleep(2)
