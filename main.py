f0200e7906fd553345df0443350928bb78c7bfaf
TheodoreDev <theodore.delbove@gmail.com>
2023-06-15 12:10:25 +0200

    Update csv atomaticly

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import csv
+import time
+
 
 dataFile = "data/data.csv"
 header = ["Username", "Favorite"]
+Username = ["Test", "Test2", "Test3"]
+Favorite = ["True", "False", "True"]
+number_user = len(Username)
 
+while True:
+    with open(dataFile, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, delimiter=",")
+        writer.writerow(header)
+        for i in range(number_user):
+            writer.writerow([f"{Username[i]}", f"{Favorite[i]}"])
 
-with open(dataFile, "w", newline="") as csv_file:
-    writer = csv.writer(csv_file, delimiter=",")
-    writer.writerow(header)
-    writer.writerow(["Test", "True"])
-    writer.writerow(["test2", "False"])
-    writer.writerow(["test3", "True"])
-
+    print("update")
+    time.sleep(2)
